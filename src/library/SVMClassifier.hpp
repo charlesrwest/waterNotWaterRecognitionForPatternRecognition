f9bcd86be9736ee392ba7ae15f85b5e32000aebe
Charles West <crwest@ncsu.edu>
2016-03-24 21:13:10 -0400

    Added includes for opencv SVM library to SVM classifier.

--- src/library/SVMClassifier.hpp
+++ src/library/SVMClassifier.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "classifierBaseClass.hpp"
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
 
 class SVMClassifier : public classifierBaseClass
 {
